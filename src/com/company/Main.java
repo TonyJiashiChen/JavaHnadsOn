5b5f276260d05129afe6c8a57db5e88dc810171b
Chen Jiashi <jche0171@student.monash.edu>
2021-02-20 09:23:16 +0800

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        Scanner s = new Scanner(System.in);
+        String n = s.nextLine();
+        System.out.println(n);
+    }
+}
